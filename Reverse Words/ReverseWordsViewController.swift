dd18c4b988581e7521008859552edb352e20adc8
heyhocombo <heyhocombo@gmail.com>
2022-04-25 20:39:54 +0300

    2504

--- Reverse Words/ReverseWordsViewController.swift
+++ Reverse Words/ReverseWordsViewController.swift
@@ -8,37 +8,47 @@
 import UIKit
 
 
-final class  ReverseWordsViewController: UIViewController {
+final class  ReverseWordsViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var reverseButton: UIButton!
     @IBOutlet var reverseTextField: UITextField!
     @IBOutlet var resultLabel: UILabel!
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        reverseTextField.delegate = self
+        reverseTextField.endEditing(true)
+    }
+    internal func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        reverseButton.setTitle("Reverse", for: .normal)
+        reverseTextField.endEditing(true)
+        return true
+    }// changing reverseButton label after user tapped return button on keyboard
+    private func reverseWolrdsInSentance(sentanse: String) -> String {
+        let sample = reverseTextField.text ?? ""
+        let sampleSentence = sample
+        let allWords = sampleSentence.components(separatedBy: " ")
+        var newSentence = ""
+        for word in allWords {
+            if newSentence != "" {
+                newSentence += " "
+            }
+            let reverseWord = String(word.reversed())
+            newSentence += reverseWord
+        }
+        return newSentence
+     }
     @IBAction func reverseButtonTapped(_ sender: UIButton) {
         let sample = reverseTextField.text ?? ""
         let sampleSentence = sample
-        sender.isSelected.toggle()
-        if sender.isSelected {
-        func reverseWolrdsInSentance(sentanse: String) -> String {
-        let allWords = sampleSentence.components(separatedBy: " ")
-        var newSentence = ""
-        for word in allWords  {
-        if newSentence != "" {
-           newSentence += " "
+        if resultLabel.text?.count == reverseTextField.text?.count {
+            self.reverseTextField.text = nil
+            self.resultLabel.text = nil
+            reverseButton.setTitle("Reverse", for: .normal)
+        }else if resultLabel.text?.count != reverseTextField.text?.count {
+            resultLabel.text = reverseWolrdsInSentance(sentanse: sampleSentence)
+            reverseButton.setTitle("Clear", for: .normal)
         }
-        let reverseWord = String(word.reversed())
-           newSentence += reverseWord
-        }
-        return newSentence
-        }
-        resultLabel.text = reverseWolrdsInSentance(sentanse: sampleSentence)
-        reverseButton.setTitle("Clear", for: .normal)
-        }
-        else { self.reverseTextField.text = nil
-               self.resultLabel.text = nil
-        reverseButton.setTitle("Reverse", for: .normal)
-        }
-        }
-        }
+    }
+}
 
     
         
